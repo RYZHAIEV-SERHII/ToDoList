d2364dc7dd88a43b3508527bd316a8b0cd18da86
Serhii_R <rsp89@i.ua>
2023-12-07 23:55:37 +0200

    ADD some modifications to program

--- ToDoList.py
+++ ToDoList.py
@@ -1,14 +1,14 @@
-
 # Creating list to store a tasks
 tasks = []
 
+
 # example task
-new_task = {
-    'task': 'Write your task here!',
-    'completed': False
-}
-tasks.append(new_task)
-print(tasks)
+# new_task = {
+#     'task': 'Write your task here!',
+#     'completed': False
+# }
+# tasks.append(new_task)
+# print(tasks)
 
 
 def add_task(tasks: list, task: str):
@@ -18,10 +18,11 @@ Function to add a task to the list of tasks.
     :param task: Description of the task to be added
     """
     tasks.append({"task": task, "completed": False})
+    save_tasks(tasks)
     print('Task added!')
 
 
-def show_task(tasks: list):
+def show_tasks(tasks: list):
     """
 Function to display all available tasks in the list of tasks
     :param tasks: List of tasks to display
@@ -35,10 +36,10 @@ Function to display all available tasks in the list of tasks
             print(f'{i}. {task['task']} - {status}')
 
 
-add_task(tasks, 'Make shopping')
-add_task(tasks, 'Write a code')
-print(tasks)
-show_task(tasks)
+# add_task(tasks, 'Make shopping')
+# add_task(tasks, 'Write a code')
+# print(tasks)
+# show_tasks(tasks)
 
 
 def complete_task(tasks: list, index: int):
@@ -47,11 +48,51 @@ Function to mark specific task in the list of tasks as completed
     :param tasks: List of tasks
     :param index: Index of task to be marked as completed
     """
-    task = tasks[index]
-    task['completed'] = True
-    print(f' Task "{task['task']}" marked as completed!')
+    if 0 <= index <= len(tasks):
+        task = tasks[index]
+        task['completed'] = True
+        save_tasks(tasks)
+        print(f'Task "{task['task']}" marked as completed!')
+    else:
+        print("Invalid index of task!\n"
+              "Please select a correct index of task you want to mark as completed")
 
 
-complete_task(tasks, 1)
-print(tasks)
-show_task(tasks)
+# complete_task(tasks, 1)
+# print(tasks)
+# show_tasks(tasks)
+
+
+def save_tasks(tasks: list):
+    """
+Function to save a list of tasks to a json file
+    :param tasks: list of tasks
+    """
+    import json
+    with open('tasks_list.json', 'w') as f:
+        json.dump(tasks, f, indent=4)
+    print('Tasks saved!')
+
+
+def load_tasks(saved_tasks='tasks_list.json'):
+    """
+Function to load a list of tasks from a json file
+    :param saved_tasks: path to saved tasks
+    :return: list of tasks
+    """
+    import json
+    import os
+
+    # Check if file with saved tasks exists
+    if os.path.exists(saved_tasks):
+        with open(saved_tasks, 'r') as f:
+            data = json.load(f)
+        print('Tasks loaded!')
+        return data
+    else:
+        print('Tasks loaded!')
+        return []
+
+# save_tasks(tasks)
+# tasks = load_tasks()
+# print(tasks)
